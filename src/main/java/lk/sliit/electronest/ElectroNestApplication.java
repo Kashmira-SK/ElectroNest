73eb7ab3b557a0b228cafccc9e402c978172e179
Kashmira-SK <kashmira_r@proton.me>
2026-07-29 13:31:02 +0530

    Scaffold Spring Boot project with web, JPA, PostgreSQL, Thymeleaf

---
+++ src/main/java/lk/sliit/electronest/ElectroNestApplication.java
@@ -0,0 +1,13 @@
+package lk.sliit.electronest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectroNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/sliit/electronest/ElectroNestApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sliit.electronest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
