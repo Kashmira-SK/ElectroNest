ae556f16e9b3380774450b62a5baef81405112c4
IT25102347 <it25102347@my.sliit.lk>
2026-09-14 11:40:03 +0530

    Implement product search and specification filters (PB-17)

--- src/main/java/lk/sliit/electronest/search/repository/ProductRepository.java
+++ src/main/java/lk/sliit/electronest/search/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package lk.sliit.electronest.search.repository;
 
 import lk.sliit.electronest.catalog.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 }
