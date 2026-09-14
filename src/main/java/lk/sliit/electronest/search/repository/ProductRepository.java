def7128800e520e13f453a0a4ff494864740125c
IT25102347 <it25102347@my.sliit.lk>
2026-09-14 09:57:03 +0530

    Update ProductRepository to use catalog Product entity

--- src/main/java/lk/sliit/electronest/search/repository/ProductRepository.java
+++ src/main/java/lk/sliit/electronest/search/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package lk.sliit.electronest.search.repository;
 
-import lk.sliit.electronest.search.model.Product;
+import lk.sliit.electronest.catalog.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
